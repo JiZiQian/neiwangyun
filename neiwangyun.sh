f448c6a82cd7303f6096faea3d5b9493abe30083
JiZiQian <jzq20015005@outlook.com>
2023-07-14 08:14:35 +0800

    update https

--- neiwangyun.sh
+++ neiwangyun.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 while true; do {
     ssh -R 80:127.0.0.1:22 sh@sh3.neiwangyun.net | grep https:// --line-buffered > neiwangyun.txt &
+    sleep 10
     cat neiwangyun.txt
     git add .
     git commit -m "update https"
